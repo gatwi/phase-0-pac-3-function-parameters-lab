5d314dc2f4b8634daaf126441e232f5ae82ab183
gatwirimurithi <faith.murithi@student.moringaschool.com>
2022-06-03 22:36:17 +0300

    commit to master

---
+++ index.js
@@ -0,0 +1,7 @@
+function logTwoValues(value1, value2) {
+    console.log(`The two values are ${value1} and ${value2}.`);
+}
+function functionIntroduction(name) {
+    // code to be executed
+    return "Hi, my name is ${name}."
+}
